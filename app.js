0426ca4cc2c22762bc216e2b1714d43c3a6308fa
mssadik <ranamia14144@gmail.com>
2023-02-06 22:15:13 +0600

    problem two is done

--- app.js
+++ app.js
@@ -1,10 +1,39 @@
 function mindGame(number){
-    let goon = number * 3;
-    let jog = goon + 10;
-    let vaag = jog / 2;
-    let biyog = vaag - 5;
-    return biyog;
+    if(isNaN(number) === true){
+        let massage = "plz input a number";
+        return massage;
+    }else{
+        let goon = number * 3;
+        let jog = goon + 10;
+        let vaag = jog / 2;
+        let biyog = vaag - 5;
+        return biyog;
+    }
 }
 
-let input = mindGame(20);
-console.log(input);
+let inputNumber = mindGame("10");
+console.log(inputNumber);
+
+
+
+
+
+
+
+
+
+function evenOdd(string){
+    if(isNaN(string) === false){
+        let massage = "plz input a string";
+        return massage;
+    }if(string.length % 2 === 0){
+        let even = "even";
+        return even;
+    }else{
+        let odd = "odd";
+        return odd;
+    }
+}
+
+let inputString = evenOdd("2000");
+console.log(inputString);
