9a375933144b3887de2fedc1971c740a86f04f7a
mssadik <ranamia14144@gmail.com>
2023-02-06 21:02:22 +0600

    first problem is soleved

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-chake commit
+function mindGame(number){
+    let goon = number * 3;
+    let jog = goon + 10;
+    let vaag = jog / 2;
+    let biyog = vaag - 5;
+    return biyog;
+}
+
+let input = mindGame(20);
+console.log(input);
